7a5b2348856edf99d1a0da7e6fcb47473d7337e2
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-08 17:31:03 +0530

    setup jest tests

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
     jussi_tag: Math.random().toString(32).substring(2, 15),
     author: {
       name: 'Arpit Patidar',
-      email: '<arpit.prdr@hotmail.com>',
+      email: 'arpit.prdr@hotmail.com',
       discord: 'arpit#8586'
     }
   }).end()
