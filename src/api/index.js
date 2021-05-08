ca71595305fce28bd78938309bb13b00e58f05bf
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-08 17:23:55 +0530

    added api status

--- src/api/index.js
+++ src/api/index.js
@@ -7,4 +7,16 @@ const router = new Router()
 router.use('/users', user)
 router.use('/auth', auth)
 
+router.get('/', (req, res) => {
+  res.json({
+    status: 'ok',
+    jussi_tag: Math.random().toString(32).substring(2, 15),
+    author: {
+      name: 'Arpit Patidar',
+      email: '<arpit.prdr@hotmail.com>',
+      discord: 'arpit#8586'
+    }
+  }).end()
+})
+
 export default router
