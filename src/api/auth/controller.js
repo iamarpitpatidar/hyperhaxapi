a04e6439c579029ec11db4c1659d1745fe542a48
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-17 19:37:45 +0530

    added token generation logic

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -1,7 +1,17 @@
-export const login = (req, res, next) => {
-  res.send('This route will login user without session and send access token')
+import { sign } from '../../services/jwt'
+import { success } from '../../services/response'
+
+export const login = ({ user }, res, next) => {
+  sign({
+    id: user.id,
+    secret: user.secret,
+    expiry: Date.now() + (1000 * 60 * 60)
+  })
+    .then((token) => ({ token, user: user.view() }))
+    .then(success(res, 201))
+    .catch(next)
 }
 
-export const logout = (req, res, next) => {
+export const logout = (req, res) => {
   res.send('This route will logout and purge the access token')
 }
