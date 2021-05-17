d6146bb3228c32e5d8b6d58f4043c76594339782
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-17 19:38:02 +0530

    feat: generate access token from '/auth'

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import { middleware as body } from 'bodymen'
 import { login, logout } from './controller'
+import { password as passwordAuth } from '../../services/passport'
 
 const router = Router()
 
-router.get('/', login)
+router.post('/',
+  body({ hardwareID: { type: String, required: true } }),
+  passwordAuth,
+  login)
 
 router.get('/logout', logout)
 
