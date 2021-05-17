2b97dbfaf1bd134c3b20b6f8b1340e80ed3ea0e4
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-18 00:56:42 +0530

    chore: removed subscriptions routes

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import main from './main'
 import auth from './auth'
 import user from './user'
 import invite from './invite'
-import subscription from './subscription'
 
 const router = new Router()
 
@@ -11,6 +10,5 @@ router.use('/', main)
 router.use('/auth', auth)
 router.use('/users', user)
 router.use('/invites', invite)
-router.use('/subscriptions', subscription)
 
 export default router
