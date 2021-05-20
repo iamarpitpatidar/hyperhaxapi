656ff348517a046274105f08da609635e2a0c761
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-20 18:02:12 +0530

    Tests: added multiple tests for purge token

--- src/api/auth/index.test.js
+++ src/api/auth/index.test.js
@@ -6,15 +6,32 @@ import { User } from '../user'
 import { verify, signSync } from '../../services/jwt'
 
 const app = server(express(), routes)
-let user, bannedUser, session
+let user, user2, bannedUser
+const session = {}
 
 beforeEach(async () => {
   user = await User.create({ username: 'iamarpit', password: 'password', hardwareID: '123456' })
+  user2 = await User.create({ username: 'user2', password: 'password', hardwareID: '1234567' })
   bannedUser = await User.create({ username: 'banned', password: 'password', status: 'banned' })
-  session = signSync({
+  session.user = signSync({
     id: user.id,
     secret: user.secret,
     expiry: Date.now() + (1000 * 60 * 60)
+  })
+  session.invalidId = signSync({
+    id: 'invalid',
+    secret: user.secret,
+    expiry: Date.now() + (1000 * 60 * 60)
+  })
+  session.invalidSecret = signSync({
+    id: user.id,
+    secret: 'invalid',
+    expiry: Date.now() + (1000 * 60 * 60)
+  })
+  session.expiredToken = signSync({
+    id: user.id,
+    secret: user.secret,
+    expiry: 1
   })
 })
 
@@ -31,6 +48,17 @@ describe('Get Access Token', () => {
     expect(typeof body.user).toBe('object')
     expect(body.user._id).toBe(user.id)
     expect(await verify(body.token)).toBeTruthy()
+  })
+  it('should set hardwareId if found null', async () => {
+    const hardwareID = '1234567'
+    const { status, body } = await request(app)
+      .post('/')
+      .auth('user2', 'password')
+      .send({ hardwareID: hardwareID })
+
+    expect(status).toEqual(201)
+    expect(typeof body).toBe('object')
+    expect(user2.hardwareID).toEqual(hardwareID)
   })
   it('should throw Bad Request - invalid username (400, user)', async () => {
     const { status, body } = await request(app)
@@ -110,13 +138,49 @@ describe('purge Access Token', () => {
   it('should purge Access Token - (200)', async () => {
     const { status, body } = await request(app)
       .delete('/token')
-      .send({ access_token: session })
+      .send({ access_token: session.user })
 
     expect(status).toBe(200)
     expect(typeof body).toBe('object')
     expect(body.message).toBe('access token has been purged')
   })
-  it('should throw Forbidden - missing Access Token (403)', async () => {
+  it('should throw UnAuthorized - (401, invalid ID Token)', async () => {
+    const { status, body } = await request(app)
+      .delete('/token')
+      .send({ access_token: session.invalidId })
+
+    expect(status).toBe(401)
+    expect(typeof body).toBe('object')
+    expect(body.message).toBe('Access Token is invalid')
+  })
+  it('should throw UnAuthorized - (401, invalid secret Token)', async () => {
+    const { status, body } = await request(app)
+      .delete('/token')
+      .send({ access_token: session.invalidSecret })
+
+    expect(status).toBe(401)
+    expect(typeof body).toBe('object')
+    expect(body.message).toBe('Access Token is invalid')
+  })
+  it('should throw UnAuthorized - (401, expired Token)', async () => {
+    const { status, body } = await request(app)
+      .delete('/token')
+      .send({ access_token: session.expiredToken })
+
+    expect(status).toBe(401)
+    expect(typeof body).toBe('object')
+    expect(body.message).toBe('Access Token is expired')
+  })
+  it('should throw Forbidden - (403, invalid Token)', async () => {
+    const { status, body } = await request(app)
+      .delete('/token')
+      .send({ access_token: 'invalid' })
+
+    expect(status).toBe(403)
+    expect(typeof body).toBe('object')
+    expect(body.message).toBe('Access Denied')
+  })
+  it('should throw Forbidden - (403, missing Access Token)', async () => {
     const { status, body } = await request(app)
       .delete('/token')
 
