c82874d97083fd65258e38b601390a65480f6904
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-21 13:34:10 +0530

    update: improved filter logic

--- src/api/invite/controller.js
+++ src/api/invite/controller.js
@@ -5,19 +5,18 @@ import { baseNamespace } from '../../config'
 import { success, error, notFound } from '../../services/response'
 
 export const index = ({ querymen: { query, select, cursor }, user }, res, next) => {
+  if (user.role === 'seller') query.createdBy = user.username
   Invite.count(query)
     .then(count => Invite.find(query, select, cursor)
       .then(invites => ({
-        rows: invites.filter(each => {
-          if (user.role === 'seller') return (each.createdBy === user.username) ? each.view(true) : false
-          else return each.view()
-        }),
+        rows: invites.filter(each => (user.role === 'seller') ? each.view(true) : each.view()),
         count
       }))
     )
     .then(success(res))
     .catch(next)
 }
+
 export const show = (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) return notFound(res)(null)
   Invite.findById(req.params.id)
@@ -29,9 +28,10 @@ export const show = (req, res, next) => {
     .then(success(res))
     .catch(next)
 }
+
 export const create = ({ bodymen: { body } }, res) => {
   Invite.create({
-    code: uuid(Math.random().toString(32).substring(2), baseNamespace),
+    code: uuid(body.orderID + Date.now(), baseNamespace),
     role: body.role,
     length: body.length,
     orderID: body.orderID
@@ -39,7 +39,7 @@ export const create = ({ bodymen: { body } }, res) => {
     .then(invite => invite.view(true))
     .then(success(res, 201))
     .catch(err => {
-      if (err.name === 'MongoError' && err.code === 11000) error(res, 'invite already exist. Please try again', 409)
+      if (err.name === 'MongoError' && err.code === 11000) error(res, '409', 'Invite already exist')
       else error(res, 500)
     })
 }
