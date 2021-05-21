b26e05557d2e5f6806e60c08e0be99d9a1d2a251
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-21 23:22:50 +0530

    new: added webhook routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import api from './api'
+import webhook from './webhook'
 
 const router = Router()
 
 router.use('/api', api)
+router.use('/webhook', webhook)
 
 export default router
