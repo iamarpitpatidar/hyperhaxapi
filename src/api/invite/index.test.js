46b3eea8ebce552b68d3cb664bb757dde954855e
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-21 12:57:26 +0530

    Tests: added multiple invites for testing

--- src/api/invite/index.test.js
+++ src/api/invite/index.test.js
@@ -8,27 +8,45 @@ import server from '../../services/express'
 import { baseNamespace } from '../../config'
 
 const app = server(express(), routes)
-let inviteByAdmin, inviteBySeller, user, seller, support, admin
+let user, seller, support, admin
 const session = {}
+const invite = {}
 
 beforeEach(async () => {
   user = await User.create({ username: 'user', password: 'password', hardwareID: '123456' })
   seller = await User.create({ username: 'seller', password: 'password', role: 'seller', hardwareID: '123457' })
   support = await User.create({ username: 'support', password: 'password', role: 'support', hardwareID: '123458' })
   admin = await User.create({ username: 'admin', password: 'password', role: 'admin', hardwareID: '123459' })
-  inviteByAdmin = await Invite.create({
+
+  // invites
+  invite.admin = await Invite.create({
     code: uuid(Math.random().toString(32).substring(2), baseNamespace),
     role: 'rust',
     length: 1,
-    orderID: 'ID12345'
+    orderID: 'ID123450'
   })
-  inviteBySeller = await Invite.create({
+  invite.admin2 = await Invite.create({
+    code: uuid(Math.random().toString(32).substring(2), baseNamespace),
+    role: 'rust',
+    length: 1,
+    orderID: 'ID123451'
+  })
+  invite.seller = await Invite.create({
     code: uuid(Math.random().toString(32).substring(2), baseNamespace),
     role: 'rust',
     length: 1,
     createdBy: seller.username,
-    orderID: 'ID12346'
+    orderID: 'ID123452'
   })
+  invite.seller2 = await Invite.create({
+    code: uuid(Math.random().toString(32).substring(2), baseNamespace),
+    role: 'rust',
+    length: 1,
+    createdBy: seller.username,
+    orderID: 'ID123453'
+  })
+
+  // sessions
   session.user = signSync({ id: user.id, secret: user.secret, expiry: Date.now() + (1000 * 60 * 60) })
   session.seller = signSync({ id: seller.id, secret: seller.secret, expiry: Date.now() + (1000 * 60 * 60) })
   session.support = signSync({ id: support.id, secret: support.secret, expiry: Date.now() + (1000 * 60 * 60) })
@@ -38,36 +56,36 @@ beforeEach(async () => {
 describe('Get Invites by ID', () => {
   it('should return Invite code - (200, admin)', async () => {
     const { status, body } = await request(app)
-      .get(`/${inviteByAdmin.id}`)
+      .get(`/${invite.admin.id}`)
       .send({ access_token: session.admin })
 
     expect(status).toEqual(200)
     expect(typeof body).toEqual('object')
     expect(body.createdBy).toEqual('admin')
-    expect(body._id).toEqual(inviteByAdmin.id)
-    expect(body.code).toEqual(inviteByAdmin.code)
+    expect(body._id).toEqual(invite.admin.id)
+    expect(body.code).toEqual(invite.admin.code)
   })
   it('should return Invite code - (200, support)', async () => {
     const { status, body } = await request(app)
-      .get(`/${inviteByAdmin.id}`)
+      .get(`/${invite.admin.id}`)
       .send({ access_token: session.support })
 
     expect(status).toEqual(200)
     expect(typeof body).toEqual('object')
     expect(body.createdBy).toEqual('admin')
-    expect(body._id).toEqual(inviteByAdmin.id)
-    expect(body.code).toEqual(inviteByAdmin.code)
+    expect(body._id).toEqual(invite.admin.id)
+    expect(body.code).toEqual(invite.admin.code)
   })
   it('should return Invite code - (200, seller)', async () => {
     const { status, body } = await request(app)
-      .get(`/${inviteBySeller.id}`)
+      .get(`/${invite.seller.id}`)
       .send({ access_token: session.seller })
 
     expect(status).toEqual(200)
     expect(typeof body).toEqual('object')
     expect(body.createdBy).toEqual('seller')
-    expect(body._id).toEqual(inviteBySeller.id)
-    expect(body.code).toEqual(inviteBySeller.code)
+    expect(body._id).toEqual(invite.seller.id)
+    expect(body.code).toEqual(invite.seller.code)
   })
   it('should throw Forbidden - (403, user)', async () => {
     const { status, body } = await request(app)
@@ -88,7 +106,7 @@ describe('Get Invites by ID', () => {
   })
   it('should throw not Found - created by someone else (404, seller)', async () => {
     const { status } = await request(app)
-      .get(`/${inviteByAdmin.id}`)
+      .get(`/${invite.admin.id}`)
       .send({ access_token: session.seller })
 
     expect(status).toEqual(404)
@@ -201,7 +219,7 @@ describe('Create Invite', () => {
       .send({
         role: 'rust',
         length: 1,
-        orderID: 'ID12345',
+        orderID: 'ID123450',
         access_token: session.admin
       })
 
