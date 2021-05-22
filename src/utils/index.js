922d96ad3e2dd07ec39e7101fbd3632bc7ea2573
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-22 23:04:42 +0530

    refactor: minor changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,1 @@
-export const requireProcessEnv = (name) => {
-  if (!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable')
-  }
-  return process.env[name]
-}
+export sellix from './sellix'
