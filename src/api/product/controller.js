7428f977d5159d98b1307c47986d18c7c014558c
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-22 23:34:51 +0530

    feat: added product purge routes

--- src/api/product/controller.js
+++ src/api/product/controller.js
@@ -1,5 +1,6 @@
 import { Product } from './index'
 import { success } from '../../services/response'
+import { sellix } from '../../utils'
 
 export const index = (req, res, next) => {
   Product.find()
@@ -7,6 +8,28 @@ export const index = (req, res, next) => {
       rows: product,
       count: product.length
     }))
+    .then(success(res))
+    .catch(next)
+}
+
+export const purge = (req, res, next) => {
+  Product.deleteMany({})
+    .then(() => sellix.getAllProducts())
+    .then(products => {
+      products.forEach(async product => {
+        console.log(product)
+        await Product.create({
+          name: product.title,
+          description: product.description,
+          image: product.image ? product.image : 'default',
+          price: product.price,
+          stock: product.stock !== -1 ? product.stock : 99999,
+          sellixID: product.uniqid,
+          gateways: product.gateways.filter(each => each).length ? product.gateways : ['FREE']
+        })
+      })
+      return { message: 'Purge completed' }
+    })
     .then(success(res))
     .catch(next)
 }
--- src/api/product/index.js
+++ src/api/product/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { index } from './controller'
+import { index, purge } from './controller'
+import { token } from '../../services/passport'
 export Product, { schema } from './model'
 
 const router = new Router()
 
 router.route('/')
   .get(index)
+  .delete(
+    token({ required: true, roles: ['admin'] }),
+    purge)
 
 export default router
